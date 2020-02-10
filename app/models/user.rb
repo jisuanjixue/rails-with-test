a830e776ee0d4bbca04581cb6b882babb80428e0
wangxiaobo <2078350663@qq.com>
2020-02-10 14:40:13 +0800

    用户密码

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :email, uniqueness: true 
   validates_format_of :email, with: /@/, on: :create, message: "电子邮箱无效"
   validates :password_digest, presence: true
