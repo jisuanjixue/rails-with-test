bca72adfd34bf096c84af2a6fa60826d09ea156b
wangxiaobo <2078350663@qq.com>
2020-02-15 11:12:41 +0800

    创建order model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
   has_secure_password
   validates :email, uniqueness: true
   validates_format_of :email, with: /@/, on: :create, message: '电子邮箱无效'
