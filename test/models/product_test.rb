58ee3d91099b826431a5b414cc7bec231c8f740f
wangxiaobo <2078350663@qq.com>
2020-02-15 10:59:24 +0800

    添加产品搜索方法

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,41 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test "通过产品名称查询" do
+    assert_equal 2, Product.filter_by_title('tv').count
+  end
+
+  test '通过产品名称查询并归类' do
+    assert_equal [products(:another_tv), products(:one)], Product.filter_by_title('tv').sort
+  end
+
+  test '大于200过滤价格并排序' do
+    assert_equal [products(:two), products(:one)],
+    Product .above_or_equal_to_price(200).sort
+  end
+
+  test '小于200过滤价格并排序' do
+    assert_equal [products(:another_tv)], 
+    Product .below_or_equal_to_price(200).sort
+  end
+
+  test '过滤 "videogame" 和价格小于 "100"' do
+    search_hash = { keyword: 'videogame', min_price: 100 }
+    assert Product.search(search_hash).empty? 
+  end
+
+  test '查询带有TV关键字的产品' do
+    search_hash = { keyword: 'tv', min_price: 50, max_price: 150 }
+    assert_equal [products(:another_tv)], Product.search(search_hash)
+  end
+
+  test '获取全部产品，默认不传参数' do
+    assert_equal Product.all.to_a, Product.search({})
+  end
+
+  test '通过产品ids参数查询' do
+    search_hash = { product_ids: [products(:one).id] }
+    assert_equal [products(:one)], Product.search(search_hash)
+  end
 end
