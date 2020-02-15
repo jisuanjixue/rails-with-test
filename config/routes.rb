19052b9cae5c5554a9f43d293ad8c1d744b04645
wangxiaobo <2078350663@qq.com>
2020-02-15 12:07:59 +0800

    order  show 操作

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :users, only: %i[show create update destroy]
       resources :tokens, only: %i[create]
       resources :products, only: %i[show create index update destroy]
-      resources :orders, only: [:index]
+      resources :orders, only: [:index, :show]
     end
   end
 end
--- test/controllers/api/v1/orders_controller_test.rb
+++ test/controllers/api/v1/orders_controller_test.rb
@@ -13,11 +13,12 @@ class Api::V1::OrdersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test '订单展示' do
-    get api_v1_orders_url,
+    get api_v1_orders_url(@order),
         headers: { Authorization: JsonWebToken.encode(user_id: @order.user_id) },
         as: :json
     assert_response :success
     json_response = JSON.parse(response.body)
-    assert_equal @order.user.orders.count, json_response['data'].count
+    include_product_attr = json_response['included'][0]['attributes']
+    assert_equal @order.products.first.title, include_product_attr['title']
   end
 end
