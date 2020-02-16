182cacd709fcaeacd70001bd5b50ed35669d6925
wangxiaobo <2078350663@qq.com>
2020-02-16 14:13:16 +0800

    订单数量改变

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,20 @@
+# frozen_string_literal: true
+
 class Order < ApplicationRecord
   belongs_to :user
   validates :total, numericality: { greater_than_or_equal_to: 0 }
   validates :total, presence: true
-  has_many :products, through: :placements 
+  has_many :products, through: :placements
   before_validation :set_total!
 
   def set_total!
     self.total = products.map(&:price).sum
+  end
+
+  def build_placements_with_product_ids_and_quantities(product_ids_and_quantities)
+    product_ids_and_quantities.each do |product_id_and_quantity|
+      placement = placements.build(product_id: product_id_and_quantity[:product_id])
+      yield placement if block_given?
+    end
   end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
@@ -5,5 +7,15 @@ class OrderTest < ActiveSupport::TestCase
     order = orders(:one)
     order.total = -1
     assert_not order.valid?
+  end
+
+  test '创建2个订单' do
+    @order.build_placements_with_product_ids_and_quantities [
+      { product_id: @product1.id, quantity: 2 },
+      { product_id: @product2.id, quantity: 3 }
+    ]
+    assert_difference('Placement.count', 2) do
+      @order.save
+    end
   end
 end
