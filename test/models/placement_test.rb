d26bdcee76fd382d6b24478bf62a3a17f214b451
wangxiaobo <2078350663@qq.com>
2020-02-16 14:18:25 +0800

    减少产品数量

--- test/models/placement_test.rb
+++ test/models/placement_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PlacementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @placement = placements(:one)
+  end
+  test '通过placement减少产品数量' do
+    product = @placement.product
+    assert_difference('product.quantity', -@placement.quantity) do
+      @placement.decrement_product_quantity! 
+    end
+  end
 end
